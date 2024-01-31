cdc4a54863b3af1a611bad9136ec258753f8b3ff
Сковорода Виталий Николаевич <skovoroda@spk.ru>
2024-01-31 10:50:48 +0500

    Доработки по улучшению распознавания заявок в письмах

--- split_by_keys.py
+++ split_by_keys.py
@@ -76,7 +76,7 @@ class Key_words():
     def find_key_words(self, text):
         text = text.lower()  # Приведение текста к нижнему регистру
         text = self.split_numbers_and_words(text).replace('*', ' ').replace('х',' ')\
-            .replace('тр.', 'труба').replace('двутавр', 'профиль').replace('угол', 'уголок')
+            .replace('тр.', 'труба').replace('двутавр', 'профиль').replace('угол ', 'уголок ')
         # text = self.preprocess_text(text)
         return self.process_order(text)
 
