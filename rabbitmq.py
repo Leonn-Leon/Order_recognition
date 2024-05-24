235bfb8cc033a8962eab43cf94f98ee85f86a9a8
Сковорода Виталий Николаевич <skovoroda@spk.ru>
2024-05-24 16:23:36 +0500

    Встроил YandexGPT для распознаваний позиций из письма

--- rabbitmq.py
+++ rabbitmq.py
@@ -152,16 +152,16 @@ class Order_recognition():
 
         async with connection:
             channel = await connection.channel()
-            queue = await channel.declare_queue(conf.first_queue)
+            queue = await channel.declare_queue(conf.first_queue, timeout=50000)
             # через partial прокидываем в наш обработчик сам канал
-            await queue.consume(partial(self.consumer, channel=channel))
+            await queue.consume(partial(self.consumer, channel=channel), timeout=50000)
             print('Слушаем очередь', flush=True)
 
 
-            queue2 = await channel.declare_queue(conf.second_queue)
-            await queue2.bind(exchange=conf.exchange, routing_key=conf.routing_key)
+            queue2 = await channel.declare_queue(conf.second_queue, timeout=50000)
+            await queue2.bind(exchange=conf.exchange, routing_key=conf.routing_key, timeout=50000)
             # через partial прокидываем в наш обработчик сам канал
-            await queue2.consume(partial(self.save_truth))
+            await queue2.consume(partial(self.save_truth), timeout=50000)
             print('Слушаем очередь2', flush=True)
             try:
                 await asyncio.Future()
