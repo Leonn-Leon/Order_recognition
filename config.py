a3b9a22842626b33513acb9ed3536d730523dac7
Сковорода Виталий Николаевич <skovoroda@spk.ru>
2024-07-17 15:55:03 +0500

    Обновление токена

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-connection_url = "amqp://ai:XRKh02eFLTKjMJErcpoy@esb-dev-rmq01.spk.ru:5672/"
+connection_url = "RMQ_AI_URL"
 # amqp://ai:XRKh02eFLTKjMJErcpoy@esb-dev-rmq01.spk.ru:5672/
 first_queue = "get_message"
 second_queue = "save_truth"
 exchange = "ai"
 routing_key = "orderrecognition.save_truth"
-Authorization_AIM = "t1.9euelZqejJjJkpiSxsnHx8aajsaSzu3rnpWaipXIy4ubmcmajJKPnpPKlpTl8_cOeiJL-e9FdC8f_t3z904oIEv570V0Lx_-zef1656Vms-ezIydyZTMypmKkseKm5qU7_zF656Vms-ezIydyZTMypmKkseKm5qU.U_wHyF_C5MecA-0PcmwZfu-Q5ck6NEo6Wvtq0Iiw5qQgMBZqRNLxX9bDepTSN23iFMR7QLZzDZJKVZahZs-aDg"
+Authorization_AIM = "t1.9euelZqOz5rOzJPOisacmsjIloyUxu3rnpWaipXIy4ubmcmajJKPnpPKlpTl8_cNOiFL-e9sXg9j_t3z901oHkv572xeD2P-zef1656VmpmOzMqOnJqUmMaNyMuNm4_M7_zF656VmpmOzMqOnJqUmMaNyMuNm4_M.b4XosXt57LTuUPvJ17dwmSmJ_wBva75nVHbdHe-eBSSXuc1BOPkycqFxxH2J2-2A3Lg5mAhI5vjuJalAwdl6Bg"
 xfolderid = "b1gob9uom3r70b2es0lp"
 gpt_version_id = 'bt1riolsv2bps3bogad6'
 gpt_url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
