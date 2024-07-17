df7f42045f67a87e3ea401f5073ca97f1ce53ef7
Сковорода Виталий Николаевич <skovoroda@spk.ru>
2024-07-17 09:06:06 +0500

    Обновление токена

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-connection_url = "RMQ_AI_URL"
+connection_url = "amqp://ai:XRKh02eFLTKjMJErcpoy@esb-dev-rmq01.spk.ru:5672/"
 # amqp://ai:XRKh02eFLTKjMJErcpoy@esb-dev-rmq01.spk.ru:5672/
 first_queue = "get_message"
 second_queue = "save_truth"
 exchange = "ai"
 routing_key = "orderrecognition.save_truth"
-Authorization_AIM = "t1.9euelZqbz82Zx5qVxsqXzo2Ny8uNje3rnpWaipXIy4ubmcmajJKPnpPKlpTl8_dDCihL-e8JNGA9_t3z9wM5JUv57wk0YD3-zef1656VmpOVyYmZl5KUj4qTxpKbnYyb7_zF656VmpOVyYmZl5KUj4qTxpKbnYyb.XqQf1p0xFG2sACbokOCWvPYzxs9oyXN48TcljPWff3rbQz11o4Axe_iBSpLUxiknBH6UtELLvqXs7RYxd-rFCA"
+Authorization_AIM = "t1.9euelZqejJjJkpiSxsnHx8aajsaSzu3rnpWaipXIy4ubmcmajJKPnpPKlpTl8_cOeiJL-e9FdC8f_t3z904oIEv570V0Lx_-zef1656Vms-ezIydyZTMypmKkseKm5qU7_zF656Vms-ezIydyZTMypmKkseKm5qU.U_wHyF_C5MecA-0PcmwZfu-Q5ck6NEo6Wvtq0Iiw5qQgMBZqRNLxX9bDepTSN23iFMR7QLZzDZJKVZahZs-aDg"
 xfolderid = "b1gob9uom3r70b2es0lp"
 gpt_version_id = 'bt1riolsv2bps3bogad6'
 gpt_url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
