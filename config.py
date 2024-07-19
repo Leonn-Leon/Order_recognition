29eac012e29ec373805a32b9b829a608983303d3
Сковорода Виталий Николаевич <skovoroda@spk.ru>
2024-07-19 11:02:22 +0500

    Обновление очередей

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 connection_url = "RMQ_AI_URL"
 # amqp://ai:XRKh02eFLTKjMJErcpoy@esb-dev-rmq01.spk.ru:5672/
-first_queue = "get_message"
-second_queue = "save_truth"
+first_queue = "get_message_2"
+second_queue = "save_truth_2"
 exchange = "ai"
 routing_key = "orderrecognition.save_truth"
 Authorization_AIM = "t1.9euelZqbkc2PnozHm5mdiZnKmomJxu3rnpWaipXIy4ubmcmajJKPnpPKlpTl9PdABRhL-e85DxaY3fT3ADQVS_nvOQ8WmM3n9euelZqem8ebzJiPyZWWi8fGzM7Ml-_8xeuelZqem8ebzJiPyZWWi8fGzM7Mlw.9Ct-kMXvWSuzaP9npu4Kk8T3PBCfmDnjUjabE9tAIiU1fIYMuDBYmvqotwF3w3tvm3rlqdnOpqwCUqzNELe5Cw"
